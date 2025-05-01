7d823b400eaf07f2734666eb19dbbbc82f482096
macoder67 <manyakaontiretse@gmail.com>
2025-05-01 17:04:47 +0200

    Create qr.js

---
+++ qr.js
@@ -0,0 +1,23 @@
+
+
+					await delay(100);
+					await Qr_Code_By_Macoder_Tech.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					MacoX_MDD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service is Currently Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await MacoX_MD_QR_CODE()
+});
+module.exports = router
