12c2f373aee66f62f8cd6eafc42ee78f27ebc85a
macoder67 <manyakaontiretse@gmail.com>
2025-05-23 16:07:53 +0200

    Update pair.js
    Signed-off-by: macoder67 <manyakaontiretse@gmail.com>

--- pair.js
+++ pair.js
@@ -28,8 +28,8 @@ function randomMegaId(length = 6, numberLength = 4) {
 async function uploadCredsToMega(credsPath) {
     try {
         const storage = await new Storage({
-            email: 'nexusxd.bot@gmail.com', // Your Mega A/c Email Here
-            password: 'malvin266' // Your Mega A/c Password Here
+            email: 'manyakaontiretse@gmail.com', // Your Mega A/c Email Here
+            password: 'macoder266' // Your Mega A/c Password Here
         }).ready;
         console.log('Mega storage initialized.');
 
